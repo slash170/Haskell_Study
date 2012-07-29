c48683f4686542229ad9b53f3459a9940198ba1a
Yutaka Konishi <slash170@gmail.com>
2012-07-29 18:25:58 +0900

    7.4章　途中まで

--- sample7.hs
+++ sample7.hs
@@ -1,10 +1,37 @@
 {-# OPTIONS -Wall #-}
 
+module Shapes
+( Point(..),
+  Shape(..),
+  area,
+  nudge,
+  baseCircle,
+  baseRect
+) where
+
 -- Type Class Definition
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+-- data Person = Person String String Int Float String String deriving (Show)
+data Person = Person { firstName :: String,
+                       lastName :: String,
+                       age :: Int,
+                       height :: Float,
+                       phoneNumber :: String,
+                       flavor :: String } deriving (Show)
+-- data Car = Car String String Int deriving(Show)
+data Car = Car { company :: String,
+                 model :: String,
+                 year :: Int
+               } deriving (Show)
+
+data Car' a b c= Car' { company' :: a,
+                 model' :: b,
+                 year' :: c
+               } deriving (Show)
 
 -- Function Definition
+-- Shape Functions
 area :: Shape -> Float
 area (Circle _ r) = pi * r ^ (2 :: Integer)
 area (Rectangle (Point x1 y1) (Point x2 y2))
@@ -20,3 +47,30 @@ baseCircle r = Circle (Point 0 0) r
 
 baseRect :: Float -> Float -> Shape
 baseRect width height = Rectangle (Point 0 0) (Point width height)
+
+-- Person Functions
+-- firstName :: Person -> String
+-- firstName (Person firstname _ _ _ _ _) = firstname
+
+-- lastName :: Person -> String
+-- lastName (Person _ lastname _ _ _ _)   = lastname
+
+-- age :: Person -> Int
+-- age (Person _ _ age _ _ _) = age
+
+-- height :: Person -> Float
+-- height (Person _ _ _ height _ _) = height
+
+-- phoneNumber :: Person -> String
+-- phoneNumber (Person _ _ _ _ number _) = number
+
+-- flavor :: Person -> String
+-- flavor (Person _ _ _ _ _ flavor) = flavor
+
+tellCar :: Car -> String
+tellCar (Car {company = c, model = m, year =y}) =
+    "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+tellCar' :: (Show a) => Car' String String a -> String
+tellCar' (Car' {company' = c, model' = m, year' = y}) =
+    "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
