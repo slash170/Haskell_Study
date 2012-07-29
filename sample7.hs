6d00c74cdbd852d9ba57509a87de9d67f87887b8
Yutaka Konishi <slash170@gmail.com>
2012-07-29 16:53:59 +0900

    警告オプション　変更 関数追加   baseCircle   baseRect

--- sample7.hs
+++ sample7.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS -Wwarn -Werror #-}
+{-# OPTIONS -Wall #-}
 
 -- Type Class Definition
 data Point = Point Float Float deriving (Show)
@@ -6,7 +6,7 @@ data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 -- Function Definition
 area :: Shape -> Float
-area (Circle _ r) = pi * r ^2
+area (Circle _ r) = pi * r ^ (2 :: Integer)
 area (Rectangle (Point x1 y1) (Point x2 y2))
     = (abs $ x2 - x1) * (abs $ y2 - y1)
 
@@ -14,3 +14,9 @@ nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b
     = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
