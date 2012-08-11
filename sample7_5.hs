961ad49473bdfd96063a45821ffc10439c737e51
Yutaka Konishi <slash170@gmail.com>
2012-08-11 19:30:29 +0900

    7.8 追加

--- sample7_5.hs
+++ sample7_5.hs
@@ -90,3 +90,16 @@ treeElem x (Node a left right)
     | x == a = True
     | x < a  = treeElem x left
     | x > a  = treeElem x right
+
+
+-- 7.8
+data TrafficLight = Red | Yellow | Green
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True
+    Yellow == Yellow = True
+    _ == _ = False
+instance Show TrafficLight where
+    show Red = "Red light!"
+    show Yellow = "Yellow light!"
+    show Green = "Green light!"
