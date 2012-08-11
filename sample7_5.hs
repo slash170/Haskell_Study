1fce715752798280a3a559961640510e93b75299
Yutaka Konishi <slash170@gmail.com>
2012-08-11 18:58:56 +0900

    7.7章　追加

--- sample7_5.hs
+++ sample7_5.hs
@@ -60,3 +60,33 @@ lockerLookup lockerNumber map = case Map.lookup lockerNumber map of
                                   Just (state, code) -> if state /= Taken
                                                         then Right code
                                                         else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+-- 7.7
+data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 :-:
+data List' a = Empty' | a :-: (List' a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 ^++
+(^++) :: List' a -> List' a -> List' a
+Empty' ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+
+-- 2分探索木
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
