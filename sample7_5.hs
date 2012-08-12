b61850f76c06fdcece6e2f39e04dc0776844cc45
Yutaka Konishi <slash170@gmail.com>
2012-08-12 20:50:33 +0900

    7.9章追加

--- sample7_5.hs
+++ sample7_5.hs
@@ -103,3 +103,37 @@ instance Show TrafficLight where
     show Red = "Red light!"
     show Yellow = "Yellow light!"
     show Green = "Green light!"
+
+-- 7.9
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _  = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing  = False
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult =
+    if yesno yesnoVal
+       then yesResult
+       else noResult
