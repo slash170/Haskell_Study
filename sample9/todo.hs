0704f3e2764093060d8215c58f0a1aa5fd5c9069
Yutaka Konishi <slash170@gmail.com>
2012-08-23 10:06:23 +0900

    bump 追加

--- sample9/todo.hs
+++ sample9/todo.hs
@@ -10,6 +10,7 @@ dispatch :: String -> [String] -> IO ()
 dispatch "add" = add
 dispatch "view" = view
 dispatch "remove" = remove
+dispatch "bump" = bump
 
 main :: IO ()
 main = do
@@ -30,8 +31,21 @@ remove :: [String] -> IO ()
 remove [fileName, numberString] = do
   contents <- readFile fileName
   let todoTasks = lines contents
-  let number = read numberString
+      number = read numberString
       newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+  outputTodo (fileName, newTodoItems)
+
+bump :: [String] -> IO ()
+bump [fileName, numberString] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      number = read numberString
+      newTodoItems = unlines $ task:(delete task todoTasks)
+          where task = (todoTasks !! number)
+  outputTodo (fileName, newTodoItems)
+
+outputTodo :: (String, String) -> IO ()
+outputTodo (fileName, newTodoItems) = do
   bracketOnError (openTempFile "." "temp")
        (\(tempName, tempHandle) -> do
           hClose tempHandle
@@ -42,3 +56,4 @@ remove [fileName, numberString] = do
           hClose tempHandle
           removeFile fileName
           renameFile tempName fileName)
+  
