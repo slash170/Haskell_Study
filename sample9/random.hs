528b36911945dc8fc1406e57cded9bfc7632ec1e
Yutaka Konishi <slash170@gmail.com>
2012-08-23 11:33:53 +0900

    9.6章 ランダム関数

---
+++ sample9/random.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS -Wall #-}
+
+import System.Random
+
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+    let (firstCoin, newGen) = random gen
+        (secondCoin, newGen') = random newGen
+        (thirdCoin, _) = random newGen'
+    in  (firstCoin, secondCoin, thirdCoin)
