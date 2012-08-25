715fb0179f1b6f06bf9cbde39051881f47fd5d5c
Yutaka Konishi <slash170@gmail.com>
2012-08-25 16:16:47 +0900

    newStdGen 追加

--- sample9/random_string.hs
+++ sample9/random_string.hs
@@ -6,5 +6,5 @@ main :: IO ()
 main = do
   gen <- getStdGen
   putStrLn $ take 20 (randomRs ('a', 'z') gen)
-  gen2 <- getStdGen
-  putStrLn $ take 20 (randomRs ('a', 'z') gen2)
+  gen' <- newStdGen
+  putStrLn $ take 20 (randomRs ('a', 'z') gen')
