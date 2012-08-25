93917b14a93f7e041172b86f452dc212673eeb8b
Yutaka Konishi <slash170@gmail.com>
2012-08-25 22:24:14 +0900

    9章完了

---
+++ sample9/bytestringcopy.hs
@@ -0,0 +1,25 @@
+{-# OPTIONS -Wall #-}
+
+import System.Environment
+import System.Directory
+import System.IO
+import Control.Exception
+import qualified Data.ByteString.Lazy as B
+
+main :: IO ()
+main = do
+  (fileName1:fileName2:_) <- getArgs
+  copy fileName1 fileName2
+
+copy :: FilePath -> FilePath -> IO ()
+copy source dest = do
+  contents <- B.readFile source
+  bracketOnError
+    (openTempFile "." "temp")
+    (\(tempName, tempHandle) -> do
+       hClose tempHandle
+       removeFile tempName)
+    (\(tempName, tempHandle) -> do
+       B.hPutStr tempHandle contents
+       hClose tempHandle
+       renameFile tempName dest)
---
+++ sample9/import_bytestring.hs
@@ -0,0 +1,3 @@
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString as S
+
