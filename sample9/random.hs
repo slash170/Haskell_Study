64ee829c7c939c3c204537b6c7f065e5e747f320
Yutaka Konishi <slash170@gmail.com>
2012-08-25 13:34:29 +0900

    finiteRandoms 追加

--- sample9/random.hs
+++ sample9/random.hs
@@ -8,3 +8,14 @@ threeCoins gen =
         (secondCoin, newGen') = random newGen
         (thirdCoin, _) = random newGen'
     in  (firstCoin, secondCoin, thirdCoin)
+
+
+randoms' :: (RandomGen g, Random a) => g -> [a]
+randoms' gen = let (value, newGen) = random gen in value:randoms' newGen
+
+finiteRandoms :: (RandomGen g, Random a) => Int -> g -> ([a], g)
+finiteRandoms 0 gen = ([], gen)
+finiteRandoms n gen =
+    let (value, newGen) = random gen
+        (restOfList, finalGen) = finiteRandoms (n-1) newGen
+    in  (value:restOfList, finalGen)
