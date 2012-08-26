92bc0b32af618b3d6cd2807770c41c7408dc88b7
Yutaka Konishi <slash170@gmail.com>
2012-08-26 23:02:29 +0900

    RPN 計算機　登録

---
+++ sample10/solveRPN.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS -Wall #-}
+
+solveRPN :: String -> Double
+solveRPN = head . foldl foldingFunction [] . words
+    where
+      foldingFunction (x:y:ys) "*" = (y * x):ys
+      foldingFunction (x:y:ys) "+" = (y + x):ys
+      foldingFunction (x:y:ys) "-" = (y - x):ys
+      foldingFunction (x:y:ys) "/" = (y / x):ys
+      foldingFunction (x:y:ys) "^" = (y ** x):ys
+      foldingFunction (x:xs) "ln" = log x:xs
+      foldingFunction xs "sum" = [sum xs]
+      foldingFunction xs numberString = read numberString:xs
