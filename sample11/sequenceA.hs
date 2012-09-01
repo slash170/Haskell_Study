7a2c4b8609f1e4cae29c76cb0bf8aa4d6f01f383
Yutaka Konishi <slash170@gmail.com>
2012-09-02 02:24:09 +0900

    11章完了

---
+++ sample11/sequenceA.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS -Wall #-}
+
+import Control.Applicative
+
+sequenceA :: (Applicative f) => [f a] -> f [a]
+sequenceA [] = pure []
+sequenceA (x:xs) = (:) <$> x <*> sequenceA xs
+
+sequenceA' :: (Applicative f) => [f a] -> f [a]
+sequenceA' = foldr (liftA2 (:)) (pure [])
