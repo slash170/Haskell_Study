295651941cc5d5ff08e993a362c9c75e1937bbe1
Yutaka Konishi <slash170@gmail.com>
2012-11-19 00:08:05 +0900

    Monoid ソース追加

---
+++ sample12/lengthcompare.hs
@@ -0,0 +1,7 @@
+import Data.Monoid
+
+lengthcompare :: String -> String -> Ordering
+lengthcompare x y = (length x `compare` length y) `mappend`
+                    (vowels x `compare` vowels y) `mappend`
+                    (x `compare` y)
+  where vowels = length . filter (`elem` "aeiou")
---
+++ sample12/pair.hs
@@ -0,0 +1,9 @@
+{-# OPTIONS -Wall #-}
+
+-- import Control.Applicative
+newtype Pair b a = Pair { getPair :: (a,b) }
+
+instance Functor (Pair c) where
+  fmap f (Pair (x,y)) = Pair (f x, y)
+
+
