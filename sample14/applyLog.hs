e713c3c2ea4f627bf02e298a2e946dd7870b1c34
Yutaka Konishi <slash170@gmail.com>
2012-11-20 00:30:22 +0900

    applylog 登録

--- sample14/applyLog.hs
+++ sample14/applyLog.hs
@@ -1,7 +1,21 @@
 {-# OPTIONS -Wall #-}
 
+import Data.Monoid
+
 isBigGang :: Int -> (Bool, String)
 isBigGang x = (x > 9, "Compared gang size to 9.")
 
 applyLog :: (a, String) -> (a -> (b,String)) -> (b,String)
 applyLog (x, log) f = let (y, newLog) = f x in (y, log ++ newLog)
+
+applyLog' :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
+applyLog' (x, log) f = let (y, newLog) = f x in (y, log `mappend` newLog)
+
+
+type Food = String
+type Price = Sum Int
+
+addDrink :: Food -> (Food, Price)
+addDrink "beans" = ("milk", Sum 25)
+addDrink "jerky" = ("whiskey", Sum 99)
+addDrink _ = ("beer", Sum 30)
